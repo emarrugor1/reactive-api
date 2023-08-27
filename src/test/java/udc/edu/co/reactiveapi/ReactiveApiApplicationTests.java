47942896f817d68594338439522d48aff71ddd05
Edwin Marrugo <edwinmarr@hotmail.com>
2023-08-27 11:59:20 -0500

    init project

---
+++ src/test/java/udc/edu/co/reactiveapi/ReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package udc.edu.co.reactiveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
