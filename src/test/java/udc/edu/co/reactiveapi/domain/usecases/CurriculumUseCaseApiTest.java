0c1d2384a8ca257c530cd08115c83b0e82f356cb
edwin <edwinmarr@hotmail.com>
2024-05-15 18:26:29 -0500

    tests and jacoco added

---
+++ src/test/java/udc/edu/co/reactiveapi/domain/usecases/CurriculumUseCaseApiTest.java
@@ -0,0 +1,15 @@
+package udc.edu.co.reactiveapi.domain.usecases;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurriculumUseCaseApiTest {
+
+    @Test
+    void someTest() {
+        String string = "";
+        assertThat(string).isEqualTo("");
+    }
+}
